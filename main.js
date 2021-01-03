1c184b35c9acf83e69ca44ba27d9fd8075b995c5
Sparsh2710 <70764422+Sparsh2710@users.noreply.github.com>
2021-01-03 10:19:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 song1 = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+
 function preload() {
     song1 = loadSound('Animals.mp3');
     song2 = loadSound('Cutting_shapes.mp3');
 }
 
 function setup() {
-    canvas = createCanvas(600, 350);
+    canvas = createCanvas(600, 440);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 
 function draw() {
-    image(video, 0, 0, 600, 350);  
+    image(video, 0, 0, 600, 440);  
 }
